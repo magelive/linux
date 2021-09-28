f28900c694d25b1e2b0a2559df7db3e53fb38fab
jiangxin <magelive.jx@foxmail.com>
2021-09-28 18:07:58 +0800

    add kernel module for the test.

---
+++ module/hello/hello.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "hello kernel.");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_INFO "bye kernel.");
+	return;
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
